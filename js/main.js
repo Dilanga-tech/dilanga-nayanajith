e6b0338a7c840aa9feb18fd57b82e5088fa6d006
Dilanga Nayanajith <80156612+Dilanga-tech@users.noreply.github.com>
2022-07-04 13:02:16 +0530

    design

--- js/main.js
+++ js/main.js
@@ -440,3 +440,26 @@ $(function() {
 
 });
 
+$(document).ready(function(){
+	$('.fatco').slick({
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		// autoplay: true,
+		autoplaySpeed: 1500,
+		arrows: false,
+		dots: false,
+		pauseOnHover: false,
+		responsive: [{
+			breakpoint: 768,
+			setting: {
+				slidesToShow: 4
+			}
+		},{
+			breakpoint: 520,
+			setting: {
+				slidesToShow: 3
+			}
+		}]
+	});
+
+});
